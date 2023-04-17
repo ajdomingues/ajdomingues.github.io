a23f21263ba09303bae49d96c1e1bf3f77a67297
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-04-17 13:36:31 -0300

    Ligar e desligar num único botão. Botão consertar.

--- portifolio/js/lampada/app.js
+++ portifolio/js/lampada/app.js
@@ -1,5 +1,4 @@
-const turnOn = document.getElementById('turnOn')
-const turnOff = document.getElementById('turnOff')
+const turnOnOff = document.getElementById('turnOff')
 const lamp = document.getElementById('lamp')
 
 function isLampBroken() {
@@ -15,15 +14,34 @@ function lampOn() {
 function lampOff() {
     if (!isLampBroken()) {
         lamp.src = './img/desligada.jpg'
+        turnOnOff.textContent = "Ligar"
     }
 }
 
 function lampBroken() {
     lamp.src = './img/quebrada.jpg'
+    turnOnOff.textContent = "Consertar"
 }
 
-turnOn.addEventListener('click', lampOn)
-turnOff.addEventListener('click', lampOff)
+function consertar() {
+    document.location.reload(true);
+    turnOnOff.textContent = "Ligar"
+}
+
+function lampOnOff() {
+    if (turnOnOff.textContent == 'Consertar') {
+        consertar()
+    }
+    if (turnOnOff.textContent == 'Ligar') {
+        lampOn()
+        turnOnOff.textContent = 'Desligar'
+    } else {
+        lampOff()
+        turnOnOff.textContent = "Ligar"
+    }
+}
+
+turnOnOff.addEventListener('click', lampOnOff)
 lamp.addEventListener('mouseover', lampOn)
 lamp.addEventListener('mouseleave', lampOff)
 lamp.addEventListener('dblclick', lampBroken)
