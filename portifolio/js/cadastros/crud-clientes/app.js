7164a5023c5ebce69530d0bb5d9bcb572690227f
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-04-28 22:19:52 -0300

    função pra limpar os campos do modal.

--- portifolio/js/cadastros/crud-clientes/app.js
+++ portifolio/js/cadastros/crud-clientes/app.js
@@ -3,15 +3,18 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
-
-const tempClient = {
-    nome: "Beltrano",
-    email: "beltrano@mail.com",
-    celular: "1191234578",
-    cidade: "Pinda"
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal')
+        .classList.remove('active')
 }
+
+// const tempClient = {
+//     nome: "Shokko",
+//     email: "Shokko@mail.com",
+//     celular: "1191234578",
+//     cidade: "Pinda"
+// }
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [] // se não houver banco, envia um array vazio
 const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
@@ -42,25 +45,71 @@ const createClient = (client) => {
 
 // }
 
-// Interação com layout
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity()
+}
 
-// const saveClient = () => {
-//     //verifica campos
-//     if (isValidFields()) {
-//         console.log("cadastrando cliente")
-//     }
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
+
+// Interação com usuário
+const saveClient = () => {
+    //verifica campos
+    if (isValidFields()) {
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value
+        }
+        createClient(client)
+        closeModal()
+        //  console.log("cadastrando cliente")
+    }
+}
+
+//criar linhas
+const createRow = (client) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+
+                <td>
+                    <button type="button" class="button green">Editar</button>
+                    <button type="button" class="button red">Excluir</button>
+                </td>
+`
+    document.querySelector('#tableClient>tbody').appendChild(newRow)
+}
+
+//limpar tabela
+const clearTable=()=>{
+    const rows = document.querySelectorAll('#tableCliente>tbody tr')
+    rows.forEach
+}
+
+// update na tabela
+const updateTable = () => {
+    const dbClient = readClient()
+    clearTable()
+    dbClient.forEach(createRow)
+}
 
 
-// }
+updateTable()
+
 
 //Eventos
-
-
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
 
-// document.getElementById('salvar')
-//     .addEventListener('click', saveClient)
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
