535e4e091f8a00c789d00abe8d1354986e99d215
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-05-11 23:29:02 -0300

    Cadastro de clientes com CEP

--- portifolio/js/cadastros/cep/app.js
+++ portifolio/js/cadastros/cep/app.js
@@ -1,13 +1,47 @@
 'use strict'
 
+const limparFormulario = () => {
+    document.getElementById('endereco').value = ''
+    document.getElementById('bairro').value = ''
+    document.getElementById('cidade').value = ''
+    document.getElementById('estado').value = ''
+}
+
+const preeencherFormulario = (endereco) => {
+    limparFormulario()
+    document.getElementById('endereco').value = endereco.logradouro
+    document.getElementById('bairro').value = endereco.bairro
+    document.getElementById('cidade').value = endereco.localidade
+    document.getElementById('estado').value = endereco.uf
+    document.getElementById('numero').focus() // depois de preencher/trazer os campos acima, foca no campo número
+
+}
+
+const ehNUmero = (numero) => /^[0-9]+$/.test(numero)
+
+const cepValido = (cep) => cep.length == 8 && ehNUmero(cep)
+
 const procuraCep = async () => {
     const cep = document.getElementById('cep').value
     const url = `http://viacep.com.br/ws/${cep}/json/`
-    // fetch(url).then(response=>response.json()).then(console.log) // executa e trás uma promise / retorno assíncrono
-    const endereco = await dados.json()
-    const dados = await fetch(url)
+
+    if (cepValido(cep)) {
+
+        const dados = await fetch(url)
+        const endereco = await dados.json()
+
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('endereco').value = 'CEP não encontrado'
+            // TODO: colocar um alert ou modal sinalizando o erro
+        } else {
+            preeencherFormulario(endereco)
+        }
+    } else {
+        document.getElementById('endereco').value = 'CEP incorreto'
+        // TODO: colocar um alert ou modal sinalizando o erro
+    }
+
 }
 
 document.getElementById('cep')
     .addEventListener('focusout', procuraCep)
-
