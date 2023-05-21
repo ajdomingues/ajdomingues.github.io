99b6b99714f0a09de8c113fae58bb899e949a45d
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-05-20 23:02:53 -0300

    Correções.

--- portifolio/js/games/chess/app.js
+++ portifolio/js/games/chess/app.js
@@ -6,10 +6,10 @@ const width = 8
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-    "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "", "",
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',    
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
@@ -20,7 +20,8 @@ function createBoard() {
         square.classList.add('square')
         square.innerHTML = startPiece
         square.setAttribute('square-id', i)
-        square.classList.add('beige')
+        // square.classList.add('beige')
+        
         gameBoard.append(square)
     })
 }
