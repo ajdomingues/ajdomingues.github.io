ec4b045b3d466fc289f31d986129fa377f8917cb
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-05-22 22:29:56 -0300

    Funções de troca de jogador.

--- portifolio/js/games/chess/app.js
+++ portifolio/js/games/chess/app.js
@@ -2,6 +2,8 @@ const gameBoard = document.querySelector('#gameboard')
 const playerDisplay = document.querySelector('#player')
 const infoDisplay = document.querySelector('#info-display')
 const width = 8
+let playerGo = "black"
+playerDisplay.textContent = "black"
 
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -19,6 +21,7 @@ function createBoard() {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
+        square.firstChild?.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         // square.classList.add('beige')
         const row = Math.floor(63 - i / 8) + 1
@@ -37,6 +40,99 @@ function createBoard() {
         }
 
         gameBoard.append(square)
+
+        const allSquares = document.querySelectorAll(".square")
+
+        allSquares.forEach(square => {
+            square.addEventListener('dragstart', dragStart)
+            square.addEventListener('dragover', dragOver)
+            square.addEventListener('drop', dragDrop)
+        })
+
+        let startPositionId
+
+        function dragStart(e) {
+            startPositionId = e.target.parentNode.getAttribute('square-id')
+            draggedElement = e.target
+        }
+
+        function dragOver(e) {
+            e.preventDefault()
+        }
+
+        function dragDrop(e) {
+            e.stopPropagation()
+            const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+            const taken = e.target.classList.contains('piece')
+            const valid = checkIfValid(e.target)
+            const opponentGo = playerGo === 'white' ? 'black' : 'white'
+            const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
+
+
+            if (correctGo) {
+                // must check this first
+                if (takenByOpponent && valid) {
+                    e.target.parentNode.append(draggedElement)
+                    e.target.remove()
+                    changePlayer()
+                    return
+                }
+
+                // then check this
+                if (taken && !takenByOpponent) {
+                    infoDisplay.textContent = "you cannot go here!"
+                    setTimeout(() => infoDisplay.textContent = "", 2000)
+                    return
+                }
+            }
+            // e.target.append(draggedElement)
+        }
+
+        function checkIfValid(target) {
+            const targetId = Number( target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
+        const startId=Number(startPositionId)
+        const piece = draggedElement.id
+        }
+
+
+
+
+        function changePlayer() {
+            if (playerGo === "black") {
+                reverseIds()
+                playerGo = "white"
+                playerDisplay.textContent = "white"
+            } else {
+                revertIds()
+                playerGo = "black"
+                playerDisplay.textContent = "black"
+            }
+            if (valid) {
+                e.target.append(draggedElement)
+                changePlayer()
+                return
+            }
+        }
+
+        function reverseIds() {
+            const allSquares = document.querySelectorAll(".square")
+            allSquares.forEach((square, i) =>
+                square.setAttribute('square-id', (width * width - 1) - i))
+        }
+
+        function revertIds() {
+            const allSquares = document.querySelectorAll(".square")
+            allSquares.forEach((square, i) =>
+                square.setAttribute('square-id', i))
+        }
+
+
+
+
+
+
+
+
     })
 }
 
