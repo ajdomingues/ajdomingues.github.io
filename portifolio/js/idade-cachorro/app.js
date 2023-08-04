87ac2e52ba40fdbeb8c563ff856ecf83cb38d286
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-08-04 17:46:50 -0300

    Adaptações.

--- portifolio/js/idade-cachorro/app.js
+++ portifolio/js/idade-cachorro/app.js
@@ -2,9 +2,15 @@ const calculate = document.getElementById('calculate')
 
 function calcYearsHuman() {
     const yearsCanine = document.getElementById('idadeC').value
-    const result = document.getElementById('result')
-    const idadeHumana = parseInt(yearsCanine) * 7
-    result.textContent = `A idade do cachorro equivale a ${idadeHumana} anos humanos`
+
+    if (yearsCanine != '') {
+        const result = document.getElementById('result')
+        const yearsHuman = parseInt(yearsCanine) * 7
+        result.textContent = `A idade do cachorro equivale a ${yearsHuman} anos humanos.`
+    } else {
+        result.textContent = 'Informe um número no campo da idade.'
+    }
+
 }
 
 calculate.addEventListener('click', calcYearsHuman)
