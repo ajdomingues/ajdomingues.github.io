21410aa207bdf2228017241c3f095d851fa2edfa
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-08-07 19:47:47 -0300

    Projeto soma de intervalos

---
+++ portifolio/js/calculadoras/soma-intervalos/app.js
@@ -0,0 +1,10 @@
+const calculate = document.getElementById('calculate')
+
+function calcInterval() {
+    const firstNumber = document.getElementById('firstNumberEnter').value
+    const secondNumber = document.getElementById('secondNumberEnter').value
+
+    
+}
+
+calculate.addEventListener('click', calcInterval)
