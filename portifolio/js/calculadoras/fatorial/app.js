1b291049d3cbf735c2279d3c77a4df9ca0c8ad49
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-08-06 23:09:20 -0300

    Calculadora de fatorial.

---
+++ portifolio/js/calculadoras/fatorial/app.js
@@ -0,0 +1,19 @@
+const calculate = document.getElementById('calculate')
+
+function calcFAtorial() {
+    const numberEnter = document.getElementById('numberEnter').value
+
+    if (numberEnter != '' && numberEnter > 0) {
+        const result = document.getElementById('result')
+        let summation = 1
+        for (let i = 0; i < numberEnter; i++) {
+            summation *= i + 1
+        }
+
+        result.textContent = summation
+    } else {
+        result.textContent = 'Entrada inválida.'
+    }
+}
+
+calculate.addEventListener('click', calcFAtorial)
