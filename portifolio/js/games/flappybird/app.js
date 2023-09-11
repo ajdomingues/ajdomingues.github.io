e0c4ffc68bf423776dac98e5ba53f585af132677
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-09-11 11:48:16 -0300

    atualizações

--- portifolio/js/games/flappybird/app.js
+++ portifolio/js/games/flappybird/app.js
@@ -1,9 +1,42 @@
-function novoElemento(tagName, className){
+function novoElemento(tagName, className) {
     const elem = document.createElement(tagName)
     elem.className = className
     return elem
 }
 
-function Barreira(reversa = false){
-    
+function Barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
 }
+
+function PardeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+    this.getX = () => parseInt(this.elemento.style.left('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clienteWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+const b = new PardeBarreiras(700, 200, 400)
+document.querySelector('[acme-flappy]').appendChild(b.elemento)
