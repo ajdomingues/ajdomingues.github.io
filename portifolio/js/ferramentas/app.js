aa32c3fc472d717bf027c8c6fedf844fd03855d1
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-09-25 16:26:30 -0300

    Atualizações

--- portifolio/js/ferramentas/app.js
+++ portifolio/js/ferramentas/app.js
@@ -1,3 +1,5 @@
-const { juntarTextos } = require('./tools')
+const { juntarTextos, converterParaBinario, arredondar } = require('./tools')
 
-console.log(juntarTextos('texto asdsadsa', 'texto 5646'))
+// console.log(juntarTextos('texto asdsadsa', 'texto 5646'))
+// console.log(converterParaBinario(34))
+console.log(arredondar(35,456469))
--- portifolio/js/ferramentas/tools.js
+++ portifolio/js/ferramentas/tools.js
@@ -7,5 +7,11 @@ module.exports = {
     },
     juntarTextos(texto1, texto2) {
         return texto1.concat(texto2)
+    },
+    converterParaBinario(num1) {
+        return num1.toString(2)
+    },    
+    arredondar(num1) {
+        return num1.toFixed(2)
     }
 }
