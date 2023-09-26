2634c9e6e69fc14d274aff4cf38e1c47ef0f9d83
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2023-09-26 16:09:19 -0300

    Atualizações.

--- portifolio/js/ferramentas/app.js
+++ portifolio/js/ferramentas/app.js
@@ -1,5 +1,6 @@
-const { juntarTextos, converterParaBinario, arredondar } = require('./tools')
+const { juntarTextos, converterParaBinario, converterParaDecimal, arredondar, romanoToInt, tudoMaiusculo } = require('./tools')
 
 // console.log(juntarTextos('texto asdsadsa', 'texto 5646'))
 // console.log(converterParaBinario(34))
-console.log(arredondar(35,456469))
+// console.log(converterParaBinario(5))
+console.log(romanoToInt(tudoMaiusculo('MMDC')))
--- portifolio/js/ferramentas/tools.js
+++ portifolio/js/ferramentas/tools.js
@@ -10,8 +10,36 @@ module.exports = {
     },
     converterParaBinario(num1) {
         return num1.toString(2)
-    },    
+    },
+    converterParaDecimal(num1) {
+        return num1.toString(10)
+    },
     arredondar(num1) {
         return num1.toFixed(2)
+    },
+    romanoToInt(romano) {
+        let n = 0;        
+        const numeros = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+
+        for (let i = 0; i < romano.length; i++) {
+            let atual = romano[i];
+            let prox = romano[i + 1];
+
+            if (prox && numeros[prox] > numeros[atual]) {
+                n -= numeros[atual];
+            } else {
+                n += numeros[atual];
+            }
+        }
+
+        return n;
     }
 }
