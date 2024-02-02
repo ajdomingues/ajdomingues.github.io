1c8cc4c0cc14b043b26b270720e44b6c6565c754
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2024-02-02 20:47:08 -0300

    Concluão jogo pedra, papel ou tesoura.

--- portifolio/js/games/rock-paper-scissor/app.js
+++ portifolio/js/games/rock-paper-scissor/app.js
@@ -1,16 +1,56 @@
-const computerChoiceDisplay = document.getElementById(' computer-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById(' result')
-const possibleChoices = document.getElementById('button')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListerner('click', () => {
-    userChoice = e.taget.id
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
+// TODO:melhorar lógica e interface. Adicionar emojis
 
 function generateComputerChoice() {
-    const randomNumber = Math.random() * possibleChoices.length
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'Empatooou!'
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'Você ganhou!'
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = 'Você perdeu!'
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors") {
+        result = 'Você ganhou!'
+    }
+    if (computerChoice === 'paper' && userChoice === "rock") {
+        result = 'Você perdeu!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock") {
+        result = 'Você ganhou!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper") {
+        result = 'Você ganhou!'
+    }
+    resultDisplay.innerHTML = result
 }
--- portifolio/js/games/rock-paper-scissor/readme.md
+++ portifolio/js/games/rock-paper-scissor/readme.md
@@ -1,3 +1,39 @@
-### Rock, Paper or Scissor
+# Rock, Paper or Scissor
+Esse faz parte dum lote de 7 jogos.
 
-Esse faz parte dum lote de 7 jogos
+## Regras básicas do jogo
+
+ Regras básicas: dado um sinal, cada um dos jogadores apresenta um elemento. Pedra perde para papel (o papel embrulha a pedra); papel perde para tesoura (esta corta o primeiro); e, finalmente, a tesoura perde para a pedra, que quebra aquela
+
+Fonte: Ciência hoje.
+
+## Funcionamento da aplicação
+
+No Pedra, Papel, tesoura o usuário vai escolher um das opções e o computador vai sortear um número aleatório entre 1 e 3 que equivale a um dos itens: Rock, Paper ou Scissor.
+
+## Funcionamento da aplicação
+
+No Pedra, Papel, tesoura o usuário vai escolher um das opções e o computador vai sortear um número aleatório entre 1 e 3 que equivale a um dos itens: Rock, Paper ou Scissor. 
+
+O sorteio vai ser pela function `generateComputerChoice` e dá pra fazer assim:
+
+```jsx
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+```
+
+Ou:
+
+```jsx
+const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+```
+
+A `getResult` vai verificar as escolhas do computador/sistema e as do usuário e fazer as comparações pra ver quem ganhou ou se houve empate - escolheram a mesma opção.
+
+| Escolha do computador | Escolha do usuário | Resultado |
+| --- | --- | --- |
+| Rock | Paper | Usuário ganhou |
+| Rock | Scissors | Computador ganhou |
+| Paper | Scissors | Usuário ganhou |
+| Paper | Rock | Computador ganhou |
+| Scissors | Rock | Usuário ganhou |
+| Scissors | Paper | Usuário ganhou |
